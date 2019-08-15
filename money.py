4a688c0ef3e131d4f43a843e94a5985bfb2d7bde
江南吴彦祖 <gli38@ucsc.edu>
2019-08-15 22:34:15 +0800

    fix some bugs

--- money.py
+++ money.py
@@ -18,7 +18,7 @@ def main():
                   newEvent = True
                elif(re.search("People", list_line[0]) or re.search("people", list_line[0])):
                   if(newEvent!=True):
-                     print("Syntax Error: People names are declared before creating a new event.")
+                     print(print_color(WARNONG,"Syntax Error: People names are declared before creating a new event."))
                      break
 
 
@@ -30,7 +30,7 @@ def main():
                   final_calculation()
                else:
                   if(len(people_list)<1):
-                     print("Syntax error: People's names undeclared.")
+                     print(print_color(WARNONG,"Syntax error: People's names undeclared."))
                      break
 
                   trans_flag = False
@@ -62,7 +62,7 @@ def interactiveMode():
          new_guy(people_list)
 
          # this is irrelevant to the program, but just for human readibility
-         event = input("Please enter event name:")
+         event = input("Please enter event name: ")
          print("\n current event: " + event + "     number of people: " + str(len(people_list)))
 
          # detailed transactions
@@ -75,7 +75,7 @@ def interactiveMode():
       elif(command=="result" or command=="Result" or command=="r" or command=="R"):
          final_calculation()
       else:
-         print("Please enter either \"new event\" or \"q\" or \"result\"")
+         print(print_color(WARNONG, "Please enter either \"new event\" or \"q\" or \"result\""))
 
 # if somebody transfer some money to another guy... (among those guys in the list)
 def transfer(sender, receiver, amount):
@@ -91,6 +91,15 @@ def pay(person, amount, people_list):
    for guy in owe_list:
       people_total[guy][person] += share_amount
 
+# if someone earns... (opposite to pay)
+def earn(person, amount, people_list):
+   global people_total
+   owe_list = people_list[:]
+   owe_list.remove(person)             # this guy doesn't own himself/herself
+   share_amount = float(amount) / len(people_list)
+   for guy in owe_list:
+      people_total[guy][person] -= share_amount
+
 # False: continue the program
 # True: throw out an error
 def detailed_transaction_file(trans_flag, people_list, user_trans):
@@ -100,20 +109,22 @@ def detailed_transaction_file(trans_flag, people_list, user_trans):
       return False
 
    try:
-      flag_transfer = (ut_list[1]=="T" or ut_list[1]=='t' or re.search("Trans", ut_list[1]) or re.search("trans", ut_list[1]) or ut_list[1]=="->")
-      flag_pay = (ut_list[1]=="P" or ut_list[1]=="p" or re.search("Pay", ut_list[1]) or re.search("pay", ut_list[1]) or ut_list[1]=="paid")
+      flag_transfer = (ut_list[1].lower()=="t" or re.search("trans", ut_list[1]) or ut_list[1]=="->")
+      flag_pay = (ut_list[1].lower()=="p" or re.search("pay", ut_list[1]) or ut_list[1].lower()=="paid")
+      flag_earn = (ut_list[1].lower()=="earns" or ut_list[1].lower()=="e")
    except:
-      print("Syntax error: \"pay\" or \"trans\" unstated.")
+      print(print_color(WARNONG,"Syntax error: \"pay\" or \"trans\" or \"earn\" unstated."))
       return True
 
    try:
       if(flag_transfer):
          transfer(ut_list[0], ut_list[2], ut_list[3])
       elif(flag_pay):
-         print(people_list)
          pay(ut_list[0], ut_list[2], people_list)
+      elif(flag_earn):
+         earn(ut_list[0], ut_list[2], people_list)
    except:
-      print("Syntax error: line argument error")
+      print(print_color(WARNONG,"Syntax error: line argument error"))
       return True
 
    return False
@@ -126,10 +137,11 @@ def detailed_transaction(trans_flag, people_list, user_trans):
       return True
 
    try:
-      flag_transfer = (ut_list[1]=="T" or ut_list[1]=='t' or re.search("Trans", ut_list[1]) or re.search("trans", ut_list[1]) or ut_list[1]=="->")
-      flag_pay = (ut_list[1]=="P" or ut_list[1]=="p" or re.search("Pay", ut_list[1]) or re.search("pay", ut_list[1]) or ut_list[1]=="paid")
+      flag_transfer = (ut_list[1].lower()=="t" or re.search("trans", ut_list[1]) or ut_list[1]=="->")
+      flag_pay = (ut_list[1].lower()=="p" or re.search("pay", ut_list[1]) or ut_list[1].lower()=="paid")
+      flag_earn = (ut_list[1].lower()=="earns" or ut_list[1].lower()=="e")
    except:
-      print("Please enter valid operations. (See README.md)")
+      print(print_color(WARNONG,"Please enter valid operations. (See README.md)"))
       return False
 
    try:
@@ -137,8 +149,10 @@ def detailed_transaction(trans_flag, people_list, user_trans):
          transfer(ut_list[0], ut_list[2], ut_list[3])
       elif(flag_pay):
          pay(ut_list[0], ut_list[2], people_list)
+      elif(flag_earn):
+         earn(ut_list[0], ut_list[2], people_list)
    except:
-      print("Syntax error: please refer to README.md for detailed instructions.")
+      print(print_color(WARNONG,"Syntax error: please refer to README.md for detailed instructions."))
 
    return False
 
@@ -165,10 +179,18 @@ def final_calculation():
 
    # finally we got there
    for folks in people_total:
-      print(folks + " " + str(people_total[folks]))
+      print(print_color(WHITE,folks + " " + str(people_total[folks])))
+
+def print_color(color, text):
+   colored_text = f"\033[{color}{text}\033[00m"
+   return colored_text
 
 # global variables
 people_total = dict(dict())                # people : money
+BLUE = '34m'
+OKGREEN = '92m'
+WARNONG = '93m'
+WHITE = '97m'
 
 if __name__ == '__main__':
    main()
